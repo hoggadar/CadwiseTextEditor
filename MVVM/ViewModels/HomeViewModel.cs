cf0a75cecaf0da9c8ba42914e36a138caf592d0d
Andrew <hoggadar@ya.ru>
2024-09-22 12:18:03 +0700

    Add project files.

---
+++ MVVM/ViewModels/HomeViewModel.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using TextEditor.MVVM.Commands;
+
+namespace TextEditor.MVVM.ViewModels
+{
+    public class HomeViewModel : ViewModelBase
+    {
+        private string _fileContent;
+
+        public ICommand OpenFileCommand { get; }
+
+        public HomeViewModel()
+        {
+            _fileContent = string.Empty;
+            OpenFileCommand = new RelayCommand(_ => OpenFile(), _ => true);
+        }
+
+        public string FileContent
+        {
+            get => _fileContent;
+            set
+            {
+                _fileContent = value;
+                OnPropertyChanged(nameof(FileContent));
+            }
+        }
+
+        private void OpenFile()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Выберите файл";
+            openFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string filePath = openFileDialog.FileName;
+                try
+                {
+                    string fileContent = File.ReadAllText(filePath);
+                    FileContent = fileContent;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка при чтении файла: {ex.Message}");
+                }
+            }
+        }
+    }
+}
---
+++ MVVM/ViewModels/MainViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Navigation;
+using Microsoft.Win32;
+using TextEditor.MVVM.Commands;
+using TextEditor.MVVM.Views;
+
+namespace TextEditor.MVVM.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private object _currentView;
+        
+        public ICommand NavigateToHome { get; }
+        public ICommand NavigateToAbout { get; }
+        
+
+        public MainViewModel()
+        {
+            _currentView = new HomePage();
+            OnPropertyChanged(nameof(CurrentView));
+            NavigateToHome = new RelayCommand(_ => CurrentView = new HomePage(), _ => true);
+            NavigateToAbout = new RelayCommand(_ => CurrentView = new AboutPage(), _ => true);
+        }
+
+        public object CurrentView
+        {
+            get => _currentView;
+            set
+            {
+                if (_currentView == value) return;
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
